c7ae7b005ad206d42b233bec07750594a8ca336b
ibnefer <alexandrealcime3@gmail.com>
2025-07-07 10:25:52 -0400

    Login, signup pages

---
+++ app/src/main/java/com/example/demounicdagroups/AuthViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.demounicdagroups
+
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel(): ViewModel(){
+
+}
---
+++ app/src/main/java/com/example/demounicdagroups/pages/HomePage.kt
@@ -0,0 +1,9 @@
+package com.example.demounicdagroups.pages
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.example.demounicdagroups.AuthViewModel
+
+@Composable
+fun HomePage(modifier: Modifier, navController: NavHostController, AuthViewModel: AuthViewModel){}
